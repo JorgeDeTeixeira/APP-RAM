c87102952dab54dfde4232c93dd03fb77ca884c3
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2023-06-21 07:56:41 -0300

    Adicionando o express a aplicação

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-const http = require("http");
-const host = `http://localhost`;
-const PORT = 3000;
+// const http = require("http");
+// const host = `http://localhost`;
+// const PORT = 3000;
+// const stats = require("./appRam.js");
+
+// const server = http.createServer((req, res) => {
+//     let url = req.url;
+
+//     if (url === `/stats`) {
+//         res.end(JSON.stringify(stats, null, 4));
+//     } else {
+//         res.end(`<h1>Seja bem vindo(a)!</h1>`);
+//     }
+// });
+
+// server.listen(PORT, console.log(`Server is running in ${host}:${PORT}`));
+
+const express = require("express");
+const app = express();
 const stats = require("./appRam.js");
 
-const server = http.createServer((req, res) => {
-    let url = req.url;
-
-    if (url === `/stats`) {
-        res.end(JSON.stringify(stats, null, 4));
-    } else {
-        res.end(`<h1>Seja bem vindo(a)!</h1>`);
-    }
+app.get("/stats", (req, res) => {
+    res.end(JSON.stringify(stats, null, 4));
 });
 
-server.listen(PORT, console.log(`Server is running in ${host}:${PORT}`));
+app.listen(3000, () => {
+    console.log("Server is running in port 3000!");
+});
